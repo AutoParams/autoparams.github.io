929bd69a9e6fe79a37c3191fb5d9305efb23cc2c
Jinho Yim <jinhoyim@outlook.com>
2024-11-03 04:25:21 +0900

    자바 코틀린 코드 블럭 컴포넌트 추가

---
+++ src/components/JavaKotlinCodeBlock/index.tsx
@@ -0,0 +1,47 @@
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
+
+type CodeInfo = {
+  lang: string,
+  label: string,
+};
+
+const languages = ['language-java', 'language-kotlin'];
+
+const langMap = new Map<string, CodeInfo>();
+langMap.set('language-java', { lang: 'java', label: 'Java'});
+langMap.set('language-kotlin', { lang: 'kotlin', label: 'Kotlin'});
+
+const extractCode = (child: JSX.Element) => child.props?.children;
+
+const JavaKotlinCodeBlock = ({children}: { children: JSX.Element[] }) => {
+  children.map(code => {
+    console.log(code);
+  });
+
+  const codes = children.map(extractCode)
+    .filter(code => languages.includes(code?.props?.className))
+    .map(code => ({
+      langKey: code.props.className,
+      children: code
+    }));
+  
+  if (codes.length === 0) {
+    return;
+  }
+
+  return (
+    <Tabs groupId="codeLanguage">
+      {codes.map((code, idx) => {
+        const codeInfo = langMap.get(code.langKey);
+        return codeInfo && (
+          <TabItem key={idx} value={codeInfo.lang} label={codeInfo.label}>
+            {code.children}
+          </TabItem>
+        );
+      })}
+    </Tabs>
+  );
+}
+
+export { JavaKotlinCodeBlock };
