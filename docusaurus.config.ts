07d082e7f465c92cd849752fee63ca7e4d59b0c0
Jinho Yim <jinhoyim@outlook.com>
2024-11-30 12:38:55 +0900

    Add Image for SEO

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -53,10 +53,12 @@ const config: Config = {
   ],
 
   themeConfig: {
+    image: 'img/autoparams.png',
     metadata: [
       { name: 'description', content: 'AutoParams is a versatile test data generator designed for parameterized tests in Java and Kotlin, drawing inspiration from AutoFixture.' },
       { name: 'google-site-verification', content: 'JTQC3Qse5xJxBr7GLaV4aYsN43IKAOO0pUhBayPB0wk'},
-      { property: 'og:site_name', content: 'AutoParams' }
+      { property: 'og:site_name', content: 'AutoParams' },
+      { property: 'og:image', content: 'https://autoparamssample.github.io/img/autoparams.png' }
     ],
     navbar: {
       title: 'AutoParams',
