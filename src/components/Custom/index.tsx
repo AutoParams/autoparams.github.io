49d5714508b4788719a32184c9574f0d8521cbf6
Jinho Yim <jinhoyim@outlook.com>
2025-05-25 18:23:41 +0900

    Extract JavadocLink component

---
+++ src/components/Custom/index.tsx
@@ -0,0 +1,4 @@
+import { JavaKotlinCodeBlock } from '@site/src/components/JavaKotlinCodeBlock';
+import { JavadocLink } from '@site/src/components/JavadocLink';
+
+export { JavaKotlinCodeBlock, JavadocLink };
